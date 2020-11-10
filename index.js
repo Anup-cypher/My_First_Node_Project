174c7e7ff37213e1f2bc324cf2752c67a1cd6ad8
anup <anuppanta5555@gmail.com>
2020-11-10 17:09:14 +0545

    creating node server using http package added

---
+++ index.js
@@ -0,0 +1,37 @@
+// const http = require('http');
+
+// let server = http.createServer((request, response)=>{
+// 	if(request.url === ' /'){
+// 		response.writeHead(200, {content-type : 'text/html'});
+// 		response.write()
+// 	}
+
+// });
+
+// server.listen(8000);
+// console.log('server is running on port', 8000);
+
+
+const http = require('http');
+
+let server = http.createServer( (request, response)=>{
+	if(request.url == '/'){
+		response.writeHead(200, {'content-type' : 'text/html'});
+		response.write('<html><body><h1> Hello World!</h1><p>This is root page</p></body></html>');
+		response.end();   //ends the response
+	}else if(request.url === '/profile'){
+		response.writeHead(200, {'content-type' : 'text/html'});
+		response.write('<html><body><h1> Hello Profile page!</h1><p>This is profile page</p></body></html>');
+		response.end();
+
+	}else{
+		response.writeHead(404, {'content-type':'text/html'});
+		response.write('<html><body><h1> 404 page not found </h1></body></html>');
+		response.end();
+	
+	}
+});
+
+
+server.listen(8000);
+console.log('Server is running on port: ', 8000);
